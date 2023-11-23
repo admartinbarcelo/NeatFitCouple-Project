2ee0f10da42a6ef5f3b01dfad55069fee67ded6e
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-11-23 10:36:02 +0100

    Add new images to assets folder

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,5 +1,9 @@
 import Logo from '../assets/logo.png';
+import welcome from '../assets/welcome-couple.png'
+import dumbell from '../assets/dumbell.png'
 
 export default {
     Logo,
+    welcome,
+    dumbell
 }
